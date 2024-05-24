5b12046d901193c811d24fd9bb8888c66ab0a6a8
AkatsukiRika <m1345860061@gmail.com>
2024-05-24 15:57:46 +0800

    build deposit fragment basic ui

---
+++ composeApp/src/commonMain/kotlin/model/records/DepositRecords.kt
@@ -0,0 +1,26 @@
+package model.records
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import model.DoubleToLongSerializer
+
+@Serializable
+data class DepositRecords(
+    val months: List<DepositMonth>
+)
+
+@Serializable
+data class DepositMonth(
+    @Serializable(with = DoubleToLongSerializer::class)
+    @SerialName("month_start_time")
+    val monthStartTime: Long,
+    @Serializable(with = DoubleToLongSerializer::class)
+    @SerialName("current_amount")
+    val currentAmount: Long,
+    @Serializable(with = DoubleToLongSerializer::class)
+    @SerialName("monthly_income")
+    val monthlyIncome: Long,
+    @Serializable(with = DoubleToLongSerializer::class)
+    @SerialName("extra_deposit")
+    val extraDeposit: Long
+)
--- composeApp/src/commonMain/kotlin/ui/fragment/DepositFragment.kt
+++ composeApp/src/commonMain/kotlin/ui/fragment/DepositFragment.kt
@@ -1,18 +1,27 @@
 package ui.fragment
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -20,14 +29,24 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import global.AppColors
 import moe.tlaster.precompose.molecule.rememberPresenter
 import moe.tlaster.precompose.navigation.Navigator
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import zhoutools.composeapp.generated.resources.Res
+import zhoutools.composeapp.generated.resources.add_monthly_record
+import zhoutools.composeapp.generated.resources.balance
+import zhoutools.composeapp.generated.resources.balance_diff
 import zhoutools.composeapp.generated.resources.current_deposit
 import zhoutools.composeapp.generated.resources.deposit
+import zhoutools.composeapp.generated.resources.extra_deposit
+import zhoutools.composeapp.generated.resources.ic_add
+import zhoutools.composeapp.generated.resources.monthly_income
 import zhoutools.composeapp.generated.resources.records
+import zhoutools.composeapp.generated.resources.total_diff
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -53,8 +72,21 @@ fun DepositFragment(navigator: Navigator) {
             fontSize = 20.sp,
             modifier = Modifier
                 .align(Alignment.Start)
-                .padding(start = 24.dp, top = 24.dp)
+                .padding(start = 24.dp, top = 24.dp, bottom = 12.dp)
         )
+
+        LazyColumn(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+        ) {
+            items(10) {
+                MonthCard()
+            }
+        }
+
+        AddRecordButton(onClick = {})
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
@@ -95,5 +127,117 @@ private fun BigCard(state: DepositState) {
 
             Spacer(modifier = Modifier.height(12.dp))
         }
+    }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun MonthCard() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 24.dp, end = 24.dp, bottom = 24.dp)
+        .clip(RoundedCornerShape(8.dp))
+        .background(Color.White)
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "May 2024",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        MonthCardDataItem(
+            type = Res.string.current_deposit,
+            value = "12345678.90"
+        )
+
+        MonthCardDataItem(
+            type = Res.string.monthly_income,
+            value = "12345.67"
+        )
+
+        MonthCardDataItem(
+            type = Res.string.balance,
+            value = "12333333.23"
+        )
+
+        MonthCardDataItem(
+            type = Res.string.extra_deposit,
+            value = "89012.34"
+        )
+
+        MonthCardDataItem(
+            type = Res.string.balance_diff,
+            value = "+12345.67",
+            valueColor = AppColors.DarkGreen
+        )
+
+        MonthCardDataItem(
+            type = Res.string.total_diff,
+            value = "-123.45",
+            valueColor = AppColors.Red
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun MonthCardDataItem(type: StringResource, value: String, valueColor: Color? = null) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 12.dp)
+    ) {
+        Text(
+            text = stringResource(type),
+            color = Color.DarkGray,
+            fontSize = 14.sp
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = value,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            color = valueColor ?: Color.Unspecified
+        )
+    }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun AddRecordButton(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(44.dp)
+            .padding(horizontal = 32.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(AppColors.LightTheme)
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.ic_add),
+            contentDescription = null,
+            tint = Color.White,
+            modifier = Modifier.size(16.dp)
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Text(
+            text = stringResource(Res.string.add_monthly_record),
+            color = Color.White
+        )
     }
 }
