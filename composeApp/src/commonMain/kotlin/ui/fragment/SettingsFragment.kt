3957dcff018310768a90ec0a02b0c37bd5470c99
AkatsukiRika <m1345860061@gmail.com>
2024-06-03 19:10:04 +0800

    feat: time card settings bottom sheet UI setup

--- composeApp/src/commonMain/kotlin/ui/fragment/SettingsFragment.kt
+++ composeApp/src/commonMain/kotlin/ui/fragment/SettingsFragment.kt
@@ -5,10 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +24,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import constant.RouteConstants
@@ -25,38 +34,46 @@ import extension.isValidUrl
 import extension.toHourMinString
 import extension.toMonthDayString
 import getAppVersion
+import global.AppColors
 import kotlinx.coroutines.launch
 import moe.tlaster.precompose.navigation.NavOptions
 import moe.tlaster.precompose.navigation.Navigator
 import moe.tlaster.precompose.navigation.PopUpTo
-import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import store.AppStore
 import ui.dialog.CloudServerDialog
 import ui.dialog.ConfirmDialog
+import ui.widget.HorizontalSeekBar
 import ui.widget.VerticalDivider
 import zhoutools.composeapp.generated.resources.Res
+import zhoutools.composeapp.generated.resources.confirm
 import zhoutools.composeapp.generated.resources.export_data
 import zhoutools.composeapp.generated.resources.ic_export
 import zhoutools.composeapp.generated.resources.ic_logout
 import zhoutools.composeapp.generated.resources.ic_server
 import zhoutools.composeapp.generated.resources.ic_sync
+import zhoutools.composeapp.generated.resources.ic_time_card
 import zhoutools.composeapp.generated.resources.invalid_url_toast
 import zhoutools.composeapp.generated.resources.last_sync_x
 import zhoutools.composeapp.generated.resources.logout
 import zhoutools.composeapp.generated.resources.logout_confirm_content
 import zhoutools.composeapp.generated.resources.logout_confirm_title
+import zhoutools.composeapp.generated.resources.min_overtime_hours
+import zhoutools.composeapp.generated.resources.min_working_hours
 import zhoutools.composeapp.generated.resources.pull
 import zhoutools.composeapp.generated.resources.push
 import zhoutools.composeapp.generated.resources.server_settings
 import zhoutools.composeapp.generated.resources.sync_confirm_content
 import zhoutools.composeapp.generated.resources.sync_confirm_title
 import zhoutools.composeapp.generated.resources.sync_data
+import zhoutools.composeapp.generated.resources.time_card_settings
 import zhoutools.composeapp.generated.resources.version_x
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsFragment(
     modifier: Modifier = Modifier,
@@ -64,6 +81,9 @@ fun SettingsFragment(
     showSnackbar: (String) -> Unit
 ) {
     val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberStandardBottomSheetState(skipHiddenState = false)
+    )
     var showLogoutDialog by remember { mutableStateOf(false) }
     var showSyncDialog by remember { mutableStateOf(false) }
     var showServerDialog by remember { mutableStateOf(false) }
@@ -82,127 +102,72 @@ fun SettingsFragment(
         )
     }
 
-    Column(
+    BottomSheetScaffold(
+        sheetContent = {
+            TimeCardBottomSheet(onConfirm = {
+                scope.launch {
+                    scaffoldState.bottomSheetState.hide()
+                }
+            })
+        },
         modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 0.dp,
+        containerColor = AppColors.Background,
+        sheetShadowElevation = 16.dp
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    showLogoutDialog = true
-                },
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_logout),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
-                    .size(26.dp),
-                tint = Color.Unspecified
-            )
-
-            Text(
-                text = stringResource(Res.string.logout) + ": ${AppStore.loginUsername}",
-                fontSize = 16.sp
-            )
-        }
-
-        VerticalDivider()
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    showSyncDialog = true
-                },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_sync),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
-                    .size(26.dp),
-                tint = Color.Unspecified
-            )
-
-            Text(
-                text = stringResource(Res.string.sync_data),
-                fontSize = 16.sp
-            )
-
-            if (AppStore.lastSync != 0L) {
-                val text = "(${stringResource(Res.string.last_sync_x, "${AppStore.lastSync.toMonthDayString()} ${AppStore.lastSync.toHourMinString()}")})"
-
-                Text(
-                    text = text,
-                    color = Color.Black.copy(alpha = 0.5f),
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(start = 8.dp)
-                )
+            LogOutItem {
+                showLogoutDialog = true
             }
-        }
 
-        VerticalDivider()
+            VerticalDivider()
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    navigator.navigate(RouteConstants.ROUTE_EXPORT)
-                },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_export),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
-                    .size(26.dp),
-                tint = Color.Unspecified
-            )
+            SettingItem(
+                icon = Res.drawable.ic_time_card,
+                name = Res.string.time_card_settings
+            ) {
+                scope.launch {
+                    scaffoldState.bottomSheetState.expand()
+                }
+            }
 
-            Text(
-                text = stringResource(Res.string.export_data),
-                fontSize = 16.sp
-            )
-        }
+            VerticalDivider()
 
-        VerticalDivider()
+            SyncItem {
+                showSyncDialog = true
+            }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    showServerDialog = true
-                },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_server),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
-                    .size(26.dp),
-                tint = Color.Unspecified
-            )
+            VerticalDivider()
+
+            SettingItem(
+                icon = Res.drawable.ic_export,
+                name = Res.string.export_data
+            ) {
+                navigator.navigate(RouteConstants.ROUTE_EXPORT)
+            }
+
+            VerticalDivider()
+
+            SettingItem(
+                icon = Res.drawable.ic_server,
+                name = Res.string.server_settings
+            ) {
+                showServerDialog = true
+            }
+
+            VerticalDivider()
 
             Text(
-                text = stringResource(Res.string.server_settings),
-                fontSize = 16.sp
+                text = stringResource(Res.string.version_x, getAppVersion()),
+                color = Color.Black.copy(alpha = 0.5f),
+                fontSize = 16.sp,
+                modifier = Modifier.padding(top = 48.dp)
             )
         }
-
-        VerticalDivider()
-
-        Text(
-            text = stringResource(Res.string.version_x, getAppVersion()),
-            color = Color.Black.copy(alpha = 0.5f),
-            fontSize = 16.sp,
-            modifier = Modifier.padding(top = 48.dp)
-        )
     }
 
     if (showLogoutDialog) {
@@ -256,5 +221,140 @@ fun SettingsFragment(
                 }
             }
         )
+    }
+}
+
+@Composable
+private fun LogOutItem(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.ic_logout),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
+                .size(26.dp),
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = stringResource(Res.string.logout) + ": ${AppStore.loginUsername}",
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+private fun SyncItem(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.ic_sync),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
+                .size(26.dp),
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = stringResource(Res.string.sync_data),
+            fontSize = 16.sp
+        )
+
+        if (AppStore.lastSync != 0L) {
+            val text = "(${stringResource(Res.string.last_sync_x, "${AppStore.lastSync.toMonthDayString()} ${AppStore.lastSync.toHourMinString()}")})"
+
+            Text(
+                text = text,
+                color = Color.Black.copy(alpha = 0.5f),
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun SettingItem(icon: DrawableResource, name: StringResource, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 13.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
+                .size(26.dp),
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = stringResource(name),
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+private fun TimeCardBottomSheet(onConfirm: () -> Unit) {
+    Column {
+        Text(
+            text = stringResource(Res.string.min_working_hours),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+
+        HorizontalSeekBar(
+            itemList = listOf("4h", "4.5h", "5h", "5.5h", "6h", "6.5h", "7h", "7.5h", "8h", "8.5h", "9h", "9.5h", "10h", "10.5h", "11h", "11.5h", "12h", "12.5h", "13h", "13.5h", "14h", "14.5h", "15h", "15.5h", "16h"),
+            itemWidth = 58.dp,
+            modifier = Modifier.padding(bottom = 12.dp, top = 8.dp)
+        )
+
+        Text(
+            text = stringResource(Res.string.min_overtime_hours),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+
+        HorizontalSeekBar(
+            itemList = listOf("0.5h", "1h", "1.5h", "2h", "2.5h", "3h", "3.5h", "4h"),
+            itemWidth = 58.dp,
+            modifier = Modifier.padding(bottom = 12.dp, top = 8.dp)
+        )
+
+        Button(
+            onClick = onConfirm,
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, bottom = 16.dp, top = 8.dp)
+                .fillMaxWidth()
+                .height(54.dp)
+                .clip(RoundedCornerShape(16.dp)),
+        ) {
+            Text(
+                text = stringResource(Res.string.confirm).uppercase(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/ui/widget/HorizontalSeekBar.kt
@@ -0,0 +1,96 @@
+package ui.widget
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import global.AppColors
+import logger
+import org.jetbrains.compose.resources.painterResource
+import zhoutools.composeapp.generated.resources.Res
+import zhoutools.composeapp.generated.resources.ic_arrow_down
+import kotlin.math.roundToInt
+
+@Composable
+fun HorizontalSeekBar(
+    modifier: Modifier = Modifier,
+    itemList: List<String>,
+    itemWidth: Dp
+) {
+    val density = LocalDensity.current
+    val toPx = { dp: Dp ->
+        density.run { dp.toPx() }
+    }
+    val toDp = { px: Float ->
+        density.run { px.toDp() }
+    }
+    var thisWidth by remember { mutableIntStateOf(0) }
+    val lazyListState = rememberLazyListState()
+    val paddingHorizontal = thisWidth / 2 - toPx(itemWidth) / 2
+    var selectIndex by remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(lazyListState.firstVisibleItemIndex, lazyListState.firstVisibleItemScrollOffset) {
+        lazyListState.apply {
+            val totalOffset = firstVisibleItemIndex * toPx(itemWidth) + firstVisibleItemScrollOffset
+            selectIndex = (totalOffset / toPx(itemWidth)).roundToInt()
+            logger.d("HorizontalSeekBar") { "firstVisibleItemIndex=$firstVisibleItemIndex, firstVisibleItemScrollOffset=$firstVisibleItemScrollOffset, totalOffset=$totalOffset (${toDp(totalOffset)}), selectIndex=$selectIndex" }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .onSizeChanged {
+                thisWidth = it.width
+            }
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.ic_arrow_down),
+            tint = AppColors.Theme,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            state = lazyListState,
+            contentPadding = PaddingValues(horizontal = if (paddingHorizontal > 0) toDp(paddingHorizontal) else 0.dp)
+        ) {
+            itemsIndexed(itemList) { index, it ->
+                Text(
+                    text = it,
+                    fontSize = if (index == selectIndex) 20.sp else 16.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = if (index == selectIndex) FontWeight.ExtraBold else FontWeight.Normal,
+                    modifier = Modifier
+                        .width(itemWidth)
+                        .padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
