b87f68aa857eeef2b804cc7e3768b3f68d205f4a
AkatsukiRika <m1345860061@gmail.com>
2025-01-03 18:54:36 +0800

    feat: added legend to BarChart

--- composeApp/src/commonMain/kotlin/ui/scene/DepositStatsScene.kt
+++ composeApp/src/commonMain/kotlin/ui/scene/DepositStatsScene.kt
@@ -1,12 +1,15 @@
 package ui.scene
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -14,6 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,6 +34,7 @@ import ui.widget.EmptyLayout
 import ui.widget.LineChart
 import ui.widget.TitleBar
 import zhoutools.composeapp.generated.resources.Res
+import zhoutools.composeapp.generated.resources.extra_deposit
 import zhoutools.composeapp.generated.resources.monthly_income
 import zhoutools.composeapp.generated.resources.no_data
 import zhoutools.composeapp.generated.resources.stats
@@ -88,6 +93,11 @@ private fun MainCharts(state: DepositStatsState) {
             textAlign = TextAlign.Center
         )
 
+        BarChartLegend(modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(top = 12.dp)
+        )
+
         BarChart(
             modifier = Modifier.padding(top = 16.dp),
             data = state.totalDepositBarData
@@ -108,6 +118,23 @@ private fun MainCharts(state: DepositStatsState) {
             modifier = Modifier.padding(top = 16.dp),
             data = state.incomeLineData,
             pointSpacing = 64.dp
+        )
+    }
+}
+
+@Composable
+private fun BarChartLegend(modifier: Modifier = Modifier) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Box(modifier = Modifier
+            .size(12.dp)
+            .background(AppColors.LightGold)
+        )
+
+        Spacer(modifier = Modifier.width(6.dp))
+
+        Text(
+            text = stringResource(Res.string.extra_deposit),
+            fontSize = 11.sp
         )
     }
 }
